6dabd913b82fc4a6bc67bdaec8d00fa6a7066d5a
Mahesh6221 <maheshpatil6221@gmail.com>
2024-10-09 23:03:14 +0530

    "ImageBazar project"

---
+++ myapp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from myapp.models import Category, Image
+
+# Register your models here.
+
+
+admin.site.register(Category)
+admin.site.register(Image)
+
+
+
+# mahesh6221
+# mahesh@gmail.com
+# Mahesh@123
